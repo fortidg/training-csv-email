db9fc3aab0f253bd0f6d31fc64babccb48ffce49
fortidg <dgooch@fortinet.com>
2024-03-20 16:48:08 +0000

    first commit

---
+++ sendmail.py
@@ -0,0 +1,38 @@
+import csv
+import smtplib
+import sys
+
+sys.path.insert(1, '/home/ubuntu/Python/')
+from emailcreds import sender_acc, sender_pass
+
+mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+
+def send_email(sender_acc, sender_pass, receiver_email, subject, message):
+    try:
+        server = mailserver
+        server.connect('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_acc, sender_pass)
+        server.sendmail(sender_acc, receiver_email, f"Subject: {subject}\n\n{message}")
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"An error occurred while sending the email: {str(e)}")
+    finally:
+        server.quit()
+
+def generate_emails(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row if present
+        for row in reader:
+            name = row[0]
+            receiver_email = row[1]
+            subject = row[2]
+            username = row[3]
+            password = row[4]
+            message = f"Hi {name},\n\nYour FortiCloud credentials are as follows:\n\nusername - {username}\npassword - {password}\n\n Please login using the URL provided in the qwiklab instructions.\n\nBest regards,\nFortinet CSE"
+            send_email(sender_acc, sender_pass, receiver_email, subject, message)
+
+# Usage
+csv_file = 'testcsv.csv'
+generate_emails(csv_file)
